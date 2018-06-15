4707ee943c7898baf0bc69a622d658cd785be887
Dawnabelle <dawn.mott@gmail.com>
2018-06-15 14:40:29 -0700

    begin work on navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
